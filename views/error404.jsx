904b3d5edf0c27f046a5ae70c81aad58edc386b7
Fadi-Faiyad <zenfaiyad@gmail.com>
2023-03-16 20:07:27 -0700

    Adding home page view

---
+++ views/error404.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('./default')
+
+function Error404 () {
+    return (
+      <Def>
+          <div>
+              <h1>404[PAGE NOT FOUND]</h1>
+          </div>
+      </Def>
+    )
+  }
+  
+
+module.exports = Error404
---
+++ views/home.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('./default')
+
+function home () {
+    return (
+      <Def>
+          <div>
+              <h1>HOME</h1>
+          </div>
+      </Def>
+    )
+  }
+  
+
+module.exports = home
